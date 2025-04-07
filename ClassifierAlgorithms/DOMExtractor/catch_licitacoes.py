e0515f0b10b1e09ab51c0f10f4f346eaa79a5291
PedroBeckers <teodoro.pedro@outlook.com>
2025-04-07 13:31:31 -0300

    classificacao das publicacoes a partir do titulo tokenizado

--- ClassifierAlgorithms/DOMExtractor/catch_licitacoes.py
+++ ClassifierAlgorithms/DOMExtractor/catch_licitacoes.py
@@ -5,9 +5,8 @@ import unicodedata
 import re
 import html
 
+#python nao reconhece modulo TF_IDF -> adicionar pythonpath manualmente no terminal
 from TF_IDF.tf_idf import tokenize
-
-# "concorrência, concurso, leilão, pregão e diálogo competitivo."
 
 # Caminho da pasta de downloads
 download_path = "./downloads"
@@ -66,9 +65,7 @@ def process_json_file(file_path):
                 title = decode_html(publicacao.get('titulo'))
                 if final_text != None:
                     text_qty += 1
-                    #print(tokenize(publicacao.get('titulo'))) #titulo nao contem palavras citadas pela prof
-                    #classification = classify_by_title()      #esperando novo metodo para classificar entre lic e nao lic
-                    text_to_file(final_text, "textos_licitacoes.txt")
+                    text_to_file(final_text, "textos_licitacoes.txt", classify_by_title(tokenize(title.lower())))
                     text_to_file(title, "titulos_licitacoes.txt")
                 else:
                     print("Texto ausente")
@@ -79,25 +76,28 @@ def process_json_file(file_path):
         diff = qty - text_qty
         print(f"Publicações que possuiam texto: {text_qty}")
         print(f"Publicações que não possuiam texto: {diff}")
+        return text_qty
 
-# Funcao para gravar texto da licitacao em um arquivo txt 
-def text_to_file(text, path):
+# Funcao para gravar texto da licitacao em um arquivo txt, parametro classification apenas existe caso text seja o texto da publicacao
+def text_to_file(text, path, classification=None):
     with open(path, "a", encoding="utf-8") as arc:
-        #arc.write(classification)
+        if classification != None:
+            arc.write(classification + "\n")
         arc.write(text + "\n---\n")
 
-'''
+#recebe titulo tokenizado e retorna "1" caso seja licitacao e "0" caso contrario
 def classify_by_title(tokens):
-   if 
-        
-        
-        
-        
-'''
+    classification_criteria = ["concorrência", "concurso", "leilão", "pregão"] # "diálogo competitivo" nao incluso pois nao tem exemplar 
+    for w in tokens:
+        for c in classification_criteria:
+            if w == c:
+                return "1"
+    return "0"
 
 # Função principal para processar os arquivos ZIP
 def process_zip_files():
     
+    text_sum = 0
     for directory in os.listdir(download_path):
         print(f"\nProcessando o diretório '{directory}'...")
         
@@ -119,10 +119,13 @@ def process_zip_files():
                     extract_path = os.path.splitext(processing_path)[0]
                     zip_ref.extractall(extract_path)
                     
-                    traverse_json_files(extract_path)
+                    text_sum += traverse_json_files(extract_path)
                                 
             else:
-                traverse_json_files(os.path.splitext(processing_path)[0])
+                text_sum += traverse_json_files(os.path.splitext(processing_path)[0])
+            
+        if text_sum >= 500:
+            return text_sum
                     
 # Iterar sobre todos json, tanto os extraidos de um zip, quanto os que nao foram extraidos             
 def traverse_json_files(path):
@@ -130,16 +133,18 @@ def traverse_json_files(path):
     item_qty = len(os.listdir(path))
     print(f"{item_qty} arquivos JSON encontrado(s).\n")
     
+    text_sum = 0
     for root, _, files in os.walk(path):
         for file in files:
             
             if file.endswith(".json"):
-                process_json_file(os.path.join(root, file))
-                
+                text_sum += process_json_file(os.path.join(root, file))
+    
+    return text_sum
                                 
 if __name__ == "__main__":
-    process_zip_files()
-    print("\nProcessamento concluído.")
+    n = process_zip_files()
+    print(f"\nProcessamento concluído. {n} textos foram gravados.")
     
     
     
